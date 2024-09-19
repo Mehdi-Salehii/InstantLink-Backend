951d79e5e2cbf26feb7f6c54df16bb9a077d87ef
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-19 22:07:02 +0330

    added socketio and connected it to server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import express, { Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import morgan from "morgan";
+//initialize express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+//setup utility middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//setup first endpoint
 app.get("/", (req: Request, res: Response) => {
+  res.status(200);
   res.send("Hello, TypeScript with Express and ES6 modules!");
 });
+//create http server and attach socketio to it
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+//start server
+server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
