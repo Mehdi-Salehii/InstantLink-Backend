1f885e063db2e203cb8b71752bb53170e86c33ae
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-20 01:42:36 +0330

    added front url to server socketio cors to prevent connection ban from server for client

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,15 @@ app.get("/", (req: Request, res: Response) => {
 });
 //create http server and attach socketio to it
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(`${socket.id}`);
 });
 //start server
 server.listen(PORT, () => {
